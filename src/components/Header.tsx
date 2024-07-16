bdb06fb9d930a1b87a0c1b23170f413c02ae0567
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2024-07-16 10:10:53 +0300

    removed old podcasts

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <section id="header" className="w-full h-auto px-10">
+            {/* Shows the new podcast cover */}
+            
+        </section>
+    )
+};
+
+export default Header;
